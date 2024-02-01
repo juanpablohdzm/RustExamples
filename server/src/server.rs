c924f1b91050bc1200ba593d5b3115d21baba438
DESKTOP-PHFAK66\juanp <juanpablohdzm95@gmail.com>
2024-02-01 16:20:55 +0100

    look into traits

--- server/src/server.rs
+++ server/src/server.rs
@@ -1,4 +1,7 @@
-use std::net::TcpListener;
+use std::{io::Read, net::TcpListener};
+use crate::http::Request;
+use std::convert::TryFrom;
+use std::convert::TryInto;
 
 pub struct Server {
     address: String,
@@ -13,13 +16,27 @@ impl Server {
     pub fn run(self) // this takes ownership of self, self will deallocate after run
     {
         let listener = TcpListener::bind(&self.address).unwrap();
-        
+        println!("Listening on address: {}", self.address);
         //infinite loop
         loop 
         {
             match listener.accept() {
-                Ok((stream, _)) => {
+                Ok((mut stream, _)) => {
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer)
+                    {
+                        Ok(_) => { 
+                            println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+                            match Request::try_from(&buffer[..]){
+                                Ok(request) => {
 
+                                },
+                                Err(e) => println!("Failed to parse a request: {}", e),
+                            }
+                            
+                        }
+                        Err(e) => println!("Failed to read from connection: {}", e),
+                    }
                 },
                 Err(e) => println!("Failed to establish a connection: {}", e),
             }
