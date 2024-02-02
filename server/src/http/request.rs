4277f2055a32c7688870d08961cc046bd2b3aec3
DESKTOP-PHFAK66\juanp <juanpablohdzm95@gmail.com>
2024-02-02 15:37:24 +0100

    Custom errors

--- server/src/http/request.rs
+++ server/src/http/request.rs
@@ -1,5 +1,9 @@
+use std::str::Utf8Error;
+use std::str;
 use super::method::Method;
 use std::convert::TryFrom;
+use std::error::Error;
+use std::fmt::{Display,Debug, Result as FmtResult, Formatter};
 
 pub struct Request {
     path: String,
@@ -9,9 +13,62 @@ pub struct Request {
 
 impl TryFrom<&[u8]> for Request {
 
-    type Error = String;
+    type Error = ParseError;
 
     fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+
+       let request = str::from_utf8(buf)?;
         unimplemented!()
     }
+}
+
+fn get_next_word(request: &str) -> Option<(&str, &str)> {
+    let mut iter = request.chars();
+
+    for (i, c) in request.chars().enumerate() {
+        if (c == ' ') {
+            return Some((&request[..i], &request[i + 1..])); // adding 1 is adding one byte not one character, this could cause issues if it is an emoji, but in this case we know is a space which is one byte
+        }
+    }
+    None
+}
+
+pub enum ParseError {
+    InvalidRequest,
+    InvalidEncoding,
+    InvalidProtocol,
+    InvalidMethod,
+}
+
+impl ParseError {
+    fn message(&self) -> &str {
+        match self {
+            Self::InvalidRequest => "Invalid Request",
+            Self::InvalidEncoding => "Invalid Encoding",
+            Self::InvalidProtocol => "Invalid Protocol",
+            Self::InvalidMethod => "Invalid Method",
+        }
+    }
+}
+
+impl From<Utf8Error> for ParseError {
+    fn from(value: Utf8Error) -> Self {
+        Self::InvalidEncoding
+    }
+}
+
+impl Display for ParseError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl Debug for ParseError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl Error for ParseError{
+
 }
