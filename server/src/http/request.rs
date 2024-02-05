9d8de78a4d6423461ef8b5453cad7d10f7804b2d
DESKTOP-PHFAK66\juanp <juanpablohdzm95@gmail.com>
2024-02-05 15:46:57 +0100

    Convert from option to result

--- server/src/http/request.rs
+++ server/src/http/request.rs
@@ -1,6 +1,6 @@
 use std::str::Utf8Error;
 use std::str;
-use super::method::Method;
+use super::method::{Method, MethodError};
 use std::convert::TryFrom;
 use std::error::Error;
 use std::fmt::{Display,Debug, Result as FmtResult, Formatter};
@@ -17,7 +17,16 @@ impl TryFrom<&[u8]> for Request {
 
     fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
 
-       let request = str::from_utf8(buf)?;
+        let request = str::from_utf8(buf)?;
+        let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?; // request variable shadowing
+        let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+
+        if protocol != "HTTP/1.1" {
+            return Err(ParseError::InvalidProtocol);
+        }
+
+        let method: Method = method.parse()?;
         unimplemented!()
     }
 }
@@ -26,7 +35,7 @@ fn get_next_word(request: &str) -> Option<(&str, &str)> {
     let mut iter = request.chars();
 
     for (i, c) in request.chars().enumerate() {
-        if (c == ' ') {
+        if (c == ' ' || c == '\r') {
             return Some((&request[..i], &request[i + 1..])); // adding 1 is adding one byte not one character, this could cause issues if it is an emoji, but in this case we know is a space which is one byte
         }
     }
@@ -48,6 +57,12 @@ impl ParseError {
             Self::InvalidProtocol => "Invalid Protocol",
             Self::InvalidMethod => "Invalid Method",
         }
+    }
+}
+
+impl From<MethodError> for ParseError {
+    fn from(_: MethodError) -> Self {
+        Self::InvalidMethod
     }
 }
 
