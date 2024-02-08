b2becae1739aa4cdbc9884ba893da86ded96fb30
DESKTOP-PHFAK66\juanp <juanpablohdzm95@gmail.com>
2024-02-08 16:01:02 +0100

    Lifetimes

--- server/src/http/request.rs
+++ server/src/http/request.rs
@@ -5,21 +5,21 @@ use std::convert::TryFrom;
 use std::error::Error;
 use std::fmt::{Display,Debug, Result as FmtResult, Formatter};
 
-pub struct Request {
-    path: String,
-    query_string: Option<String>,
+pub struct Request<'buf> {
+    path: &'buf str,
+    query_string: Option<&'buf str>,
     method: Method,
 }
 
-impl TryFrom<&[u8]> for Request {
+impl<'buf> TryFrom<&'buf [u8]> for Request<'buf> {
 
     type Error = ParseError;
 
-    fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+    fn try_from(buf: &'buf [u8]) -> Result<Request<'buf>, Self::Error> {
 
         let request = str::from_utf8(buf)?;
         let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
-        let (path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?; // request variable shadowing
+        let (mut path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?; // request variable shadowing
         let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
 
         if protocol != "HTTP/1.1" {
@@ -27,7 +27,18 @@ impl TryFrom<&[u8]> for Request {
         }
 
         let method: Method = method.parse()?;
-        unimplemented!()
+
+        let mut query_string = None;
+        if let Some(i) = path.find('?') {
+            query_string = Some(&path[i + 1..]);
+            path =  &path[..i];
+        }
+
+        Ok(Self {
+            path,
+            query_string,
+            method
+        })
     }
 }
 
