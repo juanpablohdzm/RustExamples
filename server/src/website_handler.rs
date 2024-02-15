e6ecbaba60895d17d1a0f7c81c15a190a26279f6
DESKTOP-PHFAK66\juanp <juanpablohdzm95@gmail.com>
2024-02-15 10:30:50 +0100

    Getters

--- server/src/website_handler.rs
+++ server/src/website_handler.rs
@@ -1,9 +1,23 @@
-use crate::http::{Request, Response, StatusCode};
+use crate::http::{Request, Response, StatusCode, Method};
 use super::server::Handler;
-pub  struct WebsiteHandler;
+pub  struct WebsiteHandler {
+    public_path: String
+}
+
+impl WebsiteHandler {
+    pub  fn new(public_path: String) -> Self {
+        Self {public_path}
+    }
+}
 
 impl Handler for WebsiteHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
-        Response::new(StatusCode::Ok, Some("<h1>Test</h1>".to_string()))
+        match request.method() { 
+            Method::GET => match request.path() {
+                "/" => Response::new(StatusCode::Ok, Some("<h1>Welcome</h1>".to_string())),
+                _ => Response::new(StatusCode::NotFound, None)
+            },
+            _ => Response::new(StatusCode::NotFound, None)
+        }
     }
 }
